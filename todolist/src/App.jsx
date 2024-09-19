82c55841c8a4c1d56843755fc6fcf9c9e0530d71
TeddyTjoe1990 <her21059@byui.edu>
2024-09-19 11:21:53 +0700

    edited all content

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,54 +1,53 @@
-import "./index.css";
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import "./index.css"
+import { TodoList } from "./TodoList"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
-  const [todos, setTodos] = useState([])
-  //setNewItem("")
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    //setNewItem("")
 
-  function handleSubmit(e) {
-    e.preventDefault()
+    return JSON.parse(localValue)
+  })
 
-    setTodos((CurrentTodos) => {
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
       return [
-        ...todos,
-        {id: crypto.randomUUID(), title: newItem, completed: false},
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
       ]
     })
   }
 
-  console.log(todos)
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
-      <form handleSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor>New Item</label>
-          <input 
-            value={newItem}
-            onChange={e => setNewItem(e.target.value)}
-            type="text" 
-            id="item" 
-            />
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
-      <ul classNami="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-      </ul>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
 }
